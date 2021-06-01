d208b0b0b6abff1e8951d86fe9b09e382ed4f9c8
alexisveryreal <alexmaple20@gmail.com>
2021-06-01 13:20:21 -0700

    cleaned up code for future commands

---
+++ src/commands/indexCommands.js
@@ -0,0 +1,30 @@
+const ping = require('./ping');
+
+// all of our commands will go here, each will need their require statement like above
+const commands = {
+  ping,
+};
+
+// get the guild and channel id from our config file
+const {
+  GUILD_ID,
+  BOT_CHANNEL_ID,
+} = require('../config');
+
+// only listening to messages sent in our server in our specific channel
+module.exports = async (msg) => {
+  if (msg.guild.id === GUILD_ID && msg.channel.id === BOT_CHANNEL_ID) {
+    // if our commands will have stuff after the command we split via spaces
+    const args = msg.content.split(' ');
+    // we then check fo early returns
+    if (args.length === 0 || args[0].charAt(0) !== '!') {
+      return;
+    }
+    // get the actual command and then check if its include in our commands above
+    // then invoke that command with the msg/args
+    const command = args.shift().substr(1);
+    if (Object.keys(commands).includes(command)) {
+      commands[command](msg, args);
+    }
+  }
+};
---
+++ src/config.js
@@ -0,0 +1,7 @@
+require('dotenv').config();
+
+module.exports = {
+  BOT_TOKEN: process.env.BOT_TOKEN,
+  GUILD_ID: process.env.GUILD_ID,
+  BOT_CHANNEL_ID: process.env.BOT_CHANNEL_ID,
+};
